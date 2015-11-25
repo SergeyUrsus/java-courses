79750cdb71cd5c917d32320381723267aebaa569
SergeyUrsus <sergepidchenko@gmail.com>
2015-11-25 01:12:06 +0100

    summ

--- Calculate.java
+++ Calculate.java
@@ -3,7 +3,18 @@ public class Calculate{
         System.out.println("Calculate...");
         int first = Integer.valueOf(args[0]);
         int second = Integer.valueOf(args[1]);
+
+
         int summ = first + second;
-        System.out.println("Sum" + summ);
+        System.out.println("Sum  " + summ);
+
+        int subtraction = first - second;
+        System.out.println("subtraction  " + subtraction);
+
+        int multiplication = first * second;
+        System.out.println("multiplication  " + multiplication);
+
+        int division = first / second;
+        System.out.println("division  " + division);
      }
 }
